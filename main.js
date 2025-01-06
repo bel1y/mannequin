7ede8b9cbdf2329b269e8f8edcf59549b1655f45
bel1y <132271045+bel1y@users.noreply.github.com>
2025-01-07 00:01:51 +0300

    sdsd
    fsdfsdf

---
+++ main.js
@@ -0,0 +1,6 @@
+function openingMenu(){
+    document.querySelector(".navbar-text-media").style="top:0px"
+}
+function closeMenuText() {
+        document.querySelector(".navbar-text-media").style="top:-1000px"
+}
