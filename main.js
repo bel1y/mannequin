0c1604b91a4bac8e833e6d8f86fc1e866254133a
bel1y <132271045+bel1y@users.noreply.github.com>
2025-01-07 23:32:28 +0300

    авпавп
    вапвп

--- main.js
+++ main.js
@@ -2,31 +2,8 @@ function openingMenu(){
     document.querySelector(".navbar-text-media").style="top:0px"
 }
 function closeMenuText() {
-        document.querySelector(".navbar-text-media").style="top:-1000px"
+        document.querySelector(".navbar-text-media").style="top:-100000px"
 }
 function closeMenu() {
-    document.querySelector(".navbar-text-media").style="top:-1000px"
+    document.querySelector(".navbar-text-media").style="top:-1000000px"
 }
-const slides = document.querySelector('.slides');
-const slide = document.querySelectorAll('.slide');
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-
-let currentIndex = 0;
-
-// Функция для переключения слайдов
-function showSlide(index) {
-  const slideWidth = slide[0].clientWidth;
-  slides.style.transform = `translateX(-${index * slideWidth}px)`;
-}
-
-// Обработчики кнопок
-prevButton.addEventListener('click', () => {
-  currentIndex = (currentIndex > 0) ? currentIndex - 1 : slide.length - 1;
-  showSlide(currentIndex);
-});
-
-nextButton.addEventListener('click', () => {
-  currentIndex = (currentIndex < slide.length - 1) ? currentIndex + 1 : 0;
-  showSlide(currentIndex);
-});
