64fc91242883f3cc277d638d293c14f38c7e9ad7
bel1y <132271045+bel1y@users.noreply.github.com>
2025-01-07 13:32:32 +0300

    вава
    ыаыва

--- main.js
+++ main.js
@@ -7,3 +7,26 @@ function closeMenuText() {
 function closeMenu() {
     document.querySelector(".navbar-text-media").style="top:-1000px"
 }
+const slides = document.querySelector('.slides');
+const slide = document.querySelectorAll('.slide');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+let currentIndex = 0;
+
+// Функция для переключения слайдов
+function showSlide(index) {
+  const slideWidth = slide[0].clientWidth;
+  slides.style.transform = `translateX(-${index * slideWidth}px)`;
+}
+
+// Обработчики кнопок
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex > 0) ? currentIndex - 1 : slide.length - 1;
+  showSlide(currentIndex);
+});
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex < slide.length - 1) ? currentIndex + 1 : 0;
+  showSlide(currentIndex);
+});
