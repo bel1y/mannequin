eda20ec45e6d6144cd50b7eef466cfcb62070297
bel1y <132271045+bel1y@users.noreply.github.com>
2025-01-15 20:03:59 +0300

    sdf
    sdfs

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ function closeMenuText() {
 function closeMenu() {
     document.querySelector(".navbar-text-media").style="top:-1000000px"
 }
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Отменяем стандартное поведение формы
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const phone = document.getElementById('phone').value.trim();
+    const telegram = document.getElementById('telegram').value.trim();
+    const message = document.getElementById('message').value.trim();
+    
+    if (!name || !email || !phone || !telegram || !message) {
+        document.getElementById('errorMessage').style.display = 'block';
+    } else {
+        document.getElementById('errorMessage').style.display = 'none';
+        alert('Сообщение отправлено!'); // Здесь можно добавить логику для отправки данных
+        document.getElementById('contactForm').reset(); // Сброс формы
+    }
+});
