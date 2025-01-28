b7a10a1e98d187b1591c06e786160601adf39939
bel1y <132271045+bel1y@users.noreply.github.com>
2025-01-28 17:41:31 +0300

    we
    werwef

--- main.js
+++ main.js
@@ -24,3 +24,32 @@ document.getElementById('contactForm').addEventListener('submit', function(event
         document.getElementById('contactForm').reset(); // Сброс формы
     }
 });
+
+const selected = document.querySelector('.select-selected');
+const items = document.querySelector('.select-items');
+
+selected.addEventListener('click', () => {
+    items.style.display = items.style.display === 'block' ? 'none' : 'block';
+});
+
+document.querySelectorAll('.select-items div').forEach(item => {
+    item.addEventListener('click', () => {
+        const imgSrc = item.querySelector('img').src;
+        const langText = item.textContent.trim();
+        selected.innerHTML = `<img src="${imgSrc}" alt=""> ${langText}`;
+        items.style.display = 'none';
+    });
+});
+
+document.addEventListener('click', (e) => {
+    if (!selected.contains(e.target) && !items.contains(e.target)) {
+        items.style.display = 'none';
+    }
+});
+
+function Uzbek() {
+    window.location="/Uzbekcha.html"
+}
+function Russian() {
+    window.location="/index.html"
+}
